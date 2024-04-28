56c7acaea0b211f4588476349a3909ddbb2714a4
LNS <lns.workmail@mail.ru>
2024-04-29 01:24:49 +0600

    Lesson 2

---
+++ HM/Продвинутое программирование на C++/Lesson 2/Task 1/Умный_массив/Умный_массив.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+class Smart_array {
+public:
+	Smart_array(int size) {
+		this->size = size;
+		arr = new int(size);
+	}
+
+	~Smart_array() {
+		delete[] arr;
+	}
+
+	void add_element(int a) {
+		if (i < size) {
+			arr[i] = a;
+			i++;
+		}
+		else
+			throw ("Количество элементов больше количества элементов, на которую выделена память");
+	}
+	
+	char get_element(int a) {
+		if (a >= 0 && a <= size)
+		return arr[a];
+		else throw("Неккоректный идекс");
+	}
+private:
+	int* arr;
+	int size;
+	int i = 0;
+};
+
+int main()
+{
+
+	try {
+		Smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(1) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
---
+++ HM/Продвинутое программирование на C++/Lesson 2/Task 2/Копирование_умных_массивов/Копирование_умных_массивов.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+class Smart_array {
+public:
+	Smart_array(int size) {
+		this->size = size;
+		arr = new int(size);
+	}
+
+	~Smart_array() {
+		delete[] arr;
+	}
+
+	void add_element(int a) {
+		if (i < size) {
+			arr[i] = a;
+			i++;
+		}
+		else
+			throw ("Количество элементов больше количества элементов, на которую выделена память");
+	}
+
+	char get_element(int a) {
+		if (a >= 0 && a <= size)
+			return arr[a];
+		else throw("Неккоректный идекс");
+	}
+
+	void operator=(Smart_array right) {
+
+		if (size >= right.size)
+			for (int i = 0; i < right.size; i++) {
+				arr[i] = right.arr[i];
+			}
+		else
+			throw("размер массив больше нужного");
+		
+	}
+
+private:
+	int* arr;
+	int size;
+	int i = 0;
+};
+
+int main()
+{
+	Smart_array arr(5);
+	arr.add_element(1);
+	arr.add_element(4);
+	arr.add_element(155);
+
+	Smart_array new_array(2);
+	new_array.add_element(44);
+	new_array.add_element(34);
+
+	arr = new_array;
+}
