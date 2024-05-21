d9236f2321febe8fc6dd9ace26666ded714ffaeb
LNS <lns.workmail@mail.ru>
2024-05-21 19:42:22 +0600

    Lesson 3 update Task 1

--- HM/Продвинутое программирование на C++/Lesson 3/Task 1/Возведение_в_квадрат/Возведение_в_квадрат.cpp
+++ HM/Продвинутое программирование на C++/Lesson 3/Task 1/Возведение_в_квадрат/Возведение_в_квадрат.cpp
@@ -6,12 +6,13 @@ T pow(T a) {
     return a * a;
 }
 
-template <>
-std::vector<int> pow(std::vector<int>& vec) {
+template <typename T>
+std::vector<T>& pow(std::vector<T>& vec) {
     for (int i = 0; i < vec.size(); i++)
     {
         vec[i]*= vec[i];
     }
+    return vec;
 }
 
 int main()
@@ -20,6 +21,7 @@ int main()
     std::cout << "[IN]: " << in << std::endl;
     std::cout << "[OUT]: " << pow<int>(in) << std::endl;;
 
+    
     std::vector<int> vec = { -1,  4, 8 };
     std::cout << "[IN]: ";
     for (int i = 0; i < vec.size(); i++)
@@ -28,7 +30,8 @@ int main()
     }
 
     std::cout << std::endl;
-    pow<std::vector<int>>(vec);
+
+    pow(vec);
 
     std::cout << "[OUT]: ";
     for (int i = 0; i < vec.size(); i++)
