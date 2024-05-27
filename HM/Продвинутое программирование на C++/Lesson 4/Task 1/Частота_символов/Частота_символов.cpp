5374136caf85c989f0c5eea5dd9660fe32d87afb
LNS <lns.workmail@mail.ru>
2024-05-27 15:08:54 +0600

    Lesson 4

---
+++ HM/Продвинутое программирование на C++/Lesson 4/Task 1/Частота_символов/Частота_символов.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+int main()
+{
+    std::map<char, int, std::greater<>> my_map;
+
+    std::string str;
+
+    std::cout << "[IN]: ";
+    getline(std::cin, str);
+    std::cout << std::endl;
+
+    for (int i = 0; i < str.size(); i++) {
+        my_map[str[i]]++;
+    }
+
+    for (const auto& elem : my_map) {
+        std::cout << elem.first << ": " << elem.second << std::endl;
+    }
+}
+
---
+++ HM/Продвинутое программирование на C++/Lesson 4/Task 2/Удаление_дубликатов/Удаление_дубликатов.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <set>
+
+
+int main()
+{
+    std::cout << "[IN]:" << std::endl;
+    int size, number;
+    std::cin >> size;
+
+    std::set<int,std::greater<>> my_set;
+
+    for (int i = 0; i < size; i++) {
+        std::cin >> number;
+        my_set.insert(number);
+    }
+
+    std::cout << "[OUT]:" << std::endl;
+    for (const auto& elem : my_set)
+        std::cout << elem << std::endl;
+}
