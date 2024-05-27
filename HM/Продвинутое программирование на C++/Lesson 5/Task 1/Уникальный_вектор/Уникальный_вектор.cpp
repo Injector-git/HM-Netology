b6e739bd22b3775b7e5a131dfa3ca8b92ae67607
LNS <lns.workmail@mail.ru>
2024-05-28 04:19:10 +0600

    Lesson 5

---
+++ HM/Продвинутое программирование на C++/Lesson 5/Task 1/Уникальный_вектор/Уникальный_вектор.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+    std::vector<int> vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    
+    std::cout << "[IN]: ";
+    for (int i = 0; i < vec.size();i++) {
+        std::cout << vec[i] << " ";
+    }
+
+    std::sort(vec.begin(), vec.end());
+    auto un = std::unique(vec.begin(), vec.end());;
+    vec.erase(un, vec.end());
+    std::cout << std::endl;
+
+    std::cout << "[OUT]: ";
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
+    return 0;
+}
---
+++ HM/Продвинутое программирование на C++/Lesson 5/Task 2/Печать_контейнера/Печать_контейнера.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<set>
+#include<list>
+#include<vector>
+
+template<typename T>
+void print_container(T &container) {
+    auto print = container.begin();
+    for (int i = 0; i < container.size(); i++) {
+        std::cout << *print << " ";
+        print++;
+    }
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::cout << std::endl;
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::cout << std::endl;
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
