78fc90f6f1bfbe75e926405ea9985096239a6860
LNS <lns.workmail@mail.ru>
2024-06-02 23:06:28 +0600

    Lesson 7

---
+++ HM/Продвинутое программирование на C++/Lesson 7/Task 1/Перемещение_объектов/Перемещение_объектов.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+
+
+void move_vectors(std::vector<std::string> &one, std::vector<std::string>& two) {
+	std::vector<std::string> tmp = std::move(one);
+	one = std::move(two);
+	two = std::move(two);
+}
+
+int main()
+{
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+	move_vectors(one, two);
+}
---
+++ HM/Продвинутое программирование на C++/Lesson 7/Task 2/Большие_числа/Большие_числа.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+
+class Big_integer {
+public:
+	Big_integer(std::string number){
+		str = number;
+	};
+private:
+	std::string str;
+
+};
+void move_vectors(std::vector<std::string>& one, std::vector<std::string>& two) {
+	std::vector<std::string> tmp = std::move(one);
+	one = std::move(two);
+	two = std::move(tmp);
+}
+
+int main()
+{
+	auto number1 = Big_integer("114575");
+}
