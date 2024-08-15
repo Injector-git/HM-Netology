7b437e44dd1c174391e53b041af2b4b4a1be5a4d
LNS <lns.workmail@mail.ru>
2024-08-15 21:03:26 +0600

    Course project update

--- HM/Многопоточное и асинхронное программирование/Course project/Потокобезопасная_очередь/thread_pool.cpp
+++ HM/Многопоточное и асинхронное программирование/Course project/Потокобезопасная_очередь/thread_pool.cpp
@@ -1,7 +1,7 @@
 #include "thread_pool.h"
 
 thread_pool::thread_pool() {
-	ThreadVec = new std::vector<std::thread>[4];//4 ядра
+	ThreadVec = new std::thread[4];//4 ядра
 }
 
 thread_pool::~thread_pool() {
@@ -15,7 +15,8 @@ void thread_pool::submit(std::packaged_task<void()> task) {
 }
 
 void thread_pool::work() {
-	ThreadVec[i] = { std::thread(std::move(SafeQueue.pop())) };
+	ThreadVec[i] = {std::thread(std::move(SafeQueue.pop()))};
+	ThreadVec[i].join();
 	i++;
 	if (i > 4)i = 0;
 }
--- HM/Многопоточное и асинхронное программирование/Course project/Потокобезопасная_очередь/thread_pool.h
+++ HM/Многопоточное и асинхронное программирование/Course project/Потокобезопасная_очередь/thread_pool.h
@@ -12,7 +12,7 @@ public:
 	void submit(std::packaged_task<void()> task);
 	void work();
 private:
-	std::vector<std::thread>* ThreadVec;
+	std::thread* ThreadVec;
 	safe_queue<std::packaged_task<void()>> SafeQueue;
 	int i{};//номер исполняемого потока
 };
