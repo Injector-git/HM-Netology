4a0735a864f36c1aab9b23e603b44d65211a1048
LNS <lns.workmail@mail.ru>
2024-10-25 20:50:51 +0600

    Lesson 3 Task 1

---
+++ HM/Шаблоны проектирования в C++/3_Поведенческие_шаблоны/Task 1/Pattern_command/Pattern_command.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class PrintCon: public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << message << std::endl;
+   }
+private:
+
+};
+
+class PrintFile : public LogCommand {
+public:
+    PrintFile(std::string filename) {
+        file_.open(filename);
+
+        if (!file_.is_open()) {
+            throw std::runtime_error("Не удалось открыть файл: " + filename);
+        }
+    };
+
+    void print(const std::string& message) override {
+        
+        file_ << message;
+        file_ << std::endl;
+        
+    }
+private:
+    std::ofstream file_;
+
+};
+
+void print(LogCommand& lc) {
+    lc.print("Message");
+}
+int main()
+{
+    PrintCon Con;
+    PrintFile File("Out.txt");
+
+    print(Con);
+    print(File);
+
+}
+
