c0b8fc18a8cb0f6ca2b04db49def0f581f847542
LNS <lns.workmail@mail.ru>
2024-12-23 12:22:14 +0600

    Lesson 4 update

--- HM/Qt/Lesson 4/QT_Widget/mainwindow.cpp
+++ HM/Qt/Lesson 4/QT_Widget/mainwindow.cpp
@@ -4,13 +4,30 @@
 
 #include <QPushButton>
 #include <QProgressBar>
+#include <QRadioButton>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton, &QPushButton::toggled, this, &MainWindow::on_pushButton_toggled);
+
+    ui->radioButton_1->setText("radio 1");
+    ui->radioButton_2->setText("radio 2");
+
+    ui->comboBox->addItem("1 action");
+    ui->comboBox->addItem("2 action");
+    ui->comboBox->addItem("3 action");
+    ui->comboBox->addItem("4 action");
+
+    ui->pushButton->setText("Жми");
+    ui->pushButton->setCheckable(true);
+
+    ui->progressBar->setMinimum(0);
+    //ui->progressBar->setMaximum(10);
+    ui->progressBar->setValue(0);
+    //connect(ui->pushButton, &QPushButton::toggled, this, &MainWindow::on_pushButton_toggled);
 
 }
 
@@ -25,8 +42,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_toggled(bool checked)
 {
-    if( ui->progressBar->value()>100)  ui->progressBar->setValue(0);
-    else  ui->progressBar->setValue(ui->progressBar->value()+10);
+    if( ui->progressBar->value()>=100) {
+        ui->progressBar->setValue(0);
+    }
+    else  {
+        ui->progressBar->setValue(ui->progressBar->value()+10);
+    }
 }
 
 
